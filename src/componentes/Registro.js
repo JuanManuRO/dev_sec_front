d69c316814773aa42da18d4ce7e1e69ac68e4326
valepabon <125760572+valepabon@users.noreply.github.com>
2023-03-29 17:55:32 -0500

    Update Registro.js

--- src/componentes/Registro.js
+++ src/componentes/Registro.js
@@ -35,7 +35,11 @@ const Registro = () => {
 
   function HandleSubmit() {
     const api=new Api();
-    api.postRegistro(name:{ user, password, conf_password ,email, tema_interes })
+    api.postRegistro({username: user,
+                     password: password, 
+                     password2: conf_password,
+                     email: email,
+                     tema_interes: tema_interes})
 
     let newaccount = { user, password, conf_password ,email, tema_interes }
     if (newaccount) {
