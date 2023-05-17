363df2ec74a068594cd18774cae03e1b53fcf400
Sofía Duarte <91740653+SofiDuartS@users.noreply.github.com>
2023-05-17 17:39:16 -0500

    Update api.js

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 const ip_temas = `http://18.235.214.57:3001/api/v1/topics`
 const ip_registro = `http://54.86.40.212:5000/register`
-const ip_login = `http://10.19.64.199:3000/api/auth/login`
-const ip_torneo = ``
+const ip_login = `http://34.226.45.247:3000` ///api/auth/login
+const ip_torneo = `http://34.227.157.253:3000`
 
 
 class Api{
