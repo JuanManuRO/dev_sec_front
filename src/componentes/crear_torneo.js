eb3ffd6e14c11a556a35bfeaa6a3c2e614b439fb
unknown <juanmanu.ramirez@urosario.edu.co>
2023-05-28 17:41:54 -0500

    Update crear_torneo.js
    Generar constante de prueba y agregar navegacion

--- src/componentes/crear_torneo.js
+++ src/componentes/crear_torneo.js
@@ -1,26 +1,19 @@
 import React, { useState } from 'react'
+import { useNavigate} from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
 import Stack from 'react-bootstrap/Stack';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Api from '../apis/api';
-// const temas_prueba = {
-//     python: 1,
-//     c: 2,
-//     r: 3,
-//     ruby: 4    
-// }; 
 
-const temas_prueba = {
-
-}
+const temas_prueba = {"topics":[{"_id":"64221cb1ab2fcb15c2c32fcf","name":"Ciberseguridad","createdAt":"2023-03-27T22:46:09.121Z","updatedAt":"2023-03-27T22:46:09.125Z"},{"_id":"64221cceab2fcb15c2c32fd8","name":"Analisis Forense","createdAt":"2023-03-27T22:46:38.396Z","updatedAt":"2023-03-27T22:46:38.396Z"},{"_id":"64221ce7ab2fcb15c2c32fdf","name":"Criptografia ","createdAt":"2023-03-27T22:47:03.324Z","updatedAt":"2023-03-27T22:47:03.324Z"},{"_id":"6424a9323df5fccbf33a538e","name":"IOS","createdAt":"2023-03-29T21:10:10.877Z","updatedAt":"2023-03-29T21:10:10.880Z"},{"_id":"6424a93f3df5fccbf33a5395","name":"Movil","createdAt":"2023-03-29T21:10:23.756Z","updatedAt":"2023-03-29T21:10:23.756Z"}]};
 
 function crearSelTem() {
     const api = new Api();
     const topics = api.getTopics();
     let opciones = [];
-    topics["topics"].forEach((tema)=> opciones.push(<option>{tema["name"]}</option>));
+    temas_prueba["topics"].forEach((tema)=> opciones.push(<option>{tema["name"]}</option>));
     return opciones;
 };  
 
@@ -30,7 +23,8 @@ function crearSelTem() {
 
 function CrearTorneo () {
 
-    
+    const navigate = useNavigate();
+
     const [TorDetails, setDetails] = useState({torTema: "", torPreg: "", torPart: "", torFecha: "" });
     
     const [validated, setValidated] = useState(false);
@@ -40,8 +34,11 @@ function CrearTorneo () {
         if (form.checkValidity() === false) {
             event.preventDefault();
             event.stopPropagation();
+        } else {
+            navigate("/ingresar")
         }
         setValidated(true);
+        
     };
 
     return (
